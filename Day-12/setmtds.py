20e734bced5c45076183e7eeb689ee9fcce1d452
PYT <rravikumar34@gmail.com>
2022-09-02 08:53:41 +0530

    Day-12 Updated

---
+++ Day-12/setmtds.py
@@ -0,0 +1,16 @@
+##print(dir(set))
+
+a = {12,78,95,68,4}
+b = {12,4,500,76,120,300,400}
+print(a)
+print(b)
+print(a.union(b))
+print(a)
+print(b)
+print(a.intersection(b))
+print(a)
+print(b)
+print(a.difference(b))
+print(b.difference(a))
+print(a)
+print(b)
